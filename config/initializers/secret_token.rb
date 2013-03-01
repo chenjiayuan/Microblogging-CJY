ded73c9d220d51b0acb4d26749c5faf32c9aa723
Jiayuan Chen <jiayuan.chen@oracle.com>
2013-03-01 14:49:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CjyApp::Application.config.secret_token = 'e3d285db37262cb0fb433bfd80c85d727e636e935026eb82d21051809aa0dfe1c9ee0f01651613121e50587882ab092a3ce80eb6f62a1cc54bc33df2f8ddde17'
