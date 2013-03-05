3df1779ab3aca4263ab424fbf94c8285ac98e7fd
Jiayuan Chen <jiayuan.chen@oracle.com>
2013-03-05 13:25:13 -0500

    Add user microposts ch 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,10 @@ class User < ActiveRecord::Base
   end
   
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end  
+      # This is preliminary. See "Following users" for the full implementation.
+      Micropost.where("user_id = ?", id)
+  end
+  
   
 end
 
